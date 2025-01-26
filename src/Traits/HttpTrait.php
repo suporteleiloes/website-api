065469d8bb068db52e0ab4ff08536fe6c0aae723
Tiago Felipe <tiago@tiagofelipe.com>
2025-01-26 18:34:08 -0300

    Configurado cliente para receber token

--- src/Traits/HttpTrait.php
+++ src/Traits/HttpTrait.php
@@ -63,15 +63,15 @@ trait HttpTrait
     {
         $body = json_decode($e->getResponse()->getBody(), true);
         if (isset($body['detail'])) {
-            throw new \Exception('[api]' . is_array($body['detail']) ? serialize($body['detail']) : $body['detail'], $e->getRespose()->getCode());
+            throw new \Exception('[api]' . is_array($body['detail']) ? serialize($body['detail']) : $body['detail'], $e->getResponse()->getCode());
         }
         if (isset($body['error'])) {
-            throw new \Exception('[api]' . (is_array($body['message']) ? (string)join($body['message'], ', ') : $body['message']), $e->getRespose()->getCode());
+            throw new \Exception('[api]' . (is_array($body['message']) ? (string)join($body['message'], ', ') : $body['message']), $e->getResponse()->getCode());
         }
         try {
-            throw new \Exception('[api]' . $body, $e->getRespose()->getCode());
+            throw new \Exception('[api]' . $body, $e->getResponse()->getCode());
         } catch (\Throwable $exception) {
-            throw new \Exception('[api]' . $e->getResponse()->getBody(), $e->getRespose()->getCode());
+            throw new \Exception('[api]' . $e->getResponse()->getBody(), $e->getResponse()->getCode());
         }
     }
 }
